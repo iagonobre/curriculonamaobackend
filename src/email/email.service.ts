ad72161f3e0f696b8473480e4ef65ad308cbe35c
Iago Nobre <iagonobre22@gmail.com>
2025-11-13 01:18:25 +0100

    fix: legacy errors

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -8,6 +8,7 @@ export class EmailService {
     const nodemailerTransport = createTransport({
       host: 'smtp.sendgrid.net',
       port: 465,
+      secure: true,
       auth: {
         user: 'apikey',
         pass: process.env.SENDGRID_API_KEY,
